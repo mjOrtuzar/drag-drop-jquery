c86335e022776ff5518b1a800cc032016d525734
Maria Jose Ortuzar <re4ortuzar@hotmail.com>
2018-10-06 10:56:51 -0300

    cambios feos

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
   
     var imagenPersona =$("#imagenpersona");// guardamos la imagenpersona
-    var sombreroPersona = $("#sombreropersona"); //guardamos el sombrero
+    var sombreroPersona = "<img id='sombreropersona' src='sombrero.png' alt='' srcset='' width='160' height='160'>" //guardamos el sombrero
+    var sombreroPersona2 = "<img id='sombreropersona2' src='hat.png' alt='' srcset='' width='160' height='160'>"
+    
+    var alternativas = new Array(sombreroPersona,sombreroPersona2);
+    var sombreroAzar = Math.floor(Math.random()*2);
+    var alternativaAlAzar =alternativas[sombreroAzar];
 
+    $("#alternativas").append(alternativaAlAzar);
+    
     //JQUERY UI
     //Las funciones siguentes son propias de jQuery ui y funcionan con su script propio
     //asi que guarda los script jquery-ui.min.js y jquery-ui.min.css
 
-    sombreroPersona.draggable(); //podemos tomar el elemento y moverlo a cualquier direccion
+    alternativaAlAzar.draggable(); //podemos tomar el elemento y moverlo a cualquier direccion
     imagenPersona.droppable({  //aqui determinamos sobre que elemento caera nuestro objeto y con la propiedad drop colocamos una funcion que nos dira si el objeto esta puesto o no
       drop: function( event, ui ) {
                 $( this )
@@ -18,3 +25,4 @@ $(document).ready(function(){
             }
     })
 })
+
