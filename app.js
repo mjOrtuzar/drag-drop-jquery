7cf6ea5d1dbf219c7bc83a6cce0842596207ad8b
Maria Jose Ortuzar <re4ortuzar@hotmail.com>
2018-10-09 18:30:58 -0300

    modificando array alternativas

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
   
     var imagenPersona =$("#imagenpersona");// guardamos la imagenpersona
-    var sombreroPersona = "<img id='sombreropersona' src='sombrero.png' alt='' srcset='' width='160' height='160'>" //guardamos el sombrero
-    var sombreroPersona2 = "<img id='sombreropersona2' src='hat.png' alt='' srcset='' width='160' height='160'>"
+    var sombreropersona =$("#alternativas"); // guardamos el div alternativas
+    var alternativas = new Array(); //creamos un array que contendra las alternativas
+    alternativas[0]= "sombrero" // insertamos los objetos dentro del array
+    alternativas[1]= "hat"      // indicando su posicion.
+    var azar = Math.floor(Math.random()*2);// creamos la variable azar para mostrar las imagenes de manera aleatoria
+    var imagenes= alternativas[azar]; // usaremos esta variable imagenes para mostrar la imagen aleatoria
+    sombreropersona.append("<img src='./" + imagenes + ".png' width='160' height='160'>")
+    //dentro del div 'apendeamos' la variable imagenes dentro de una etiqueta img para mostrar las imagenes
     
-    var alternativas = new Array(sombreroPersona,sombreroPersona2);
-    var sombreroAzar = Math.floor(Math.random()*2);
-    var alternativaAlAzar =alternativas[sombreroAzar];
-
-    $("#alternativas").append(alternativaAlAzar);
-    
-    //JQUERY UI
+    //JQUERY
     //Las funciones siguentes son propias de jQuery ui y funcionan con su script propio
     //asi que guarda los script jquery-ui.min.js y jquery-ui.min.css
 
-    alternativaAlAzar.draggable(); //podemos tomar el elemento y moverlo a cualquier direccion
+    sombreropersona.draggable(); //podemos tomar el elemento y moverlo a cualquier direccion
     imagenPersona.droppable({  //aqui determinamos sobre que elemento caera nuestro objeto y con la propiedad drop colocamos una funcion que nos dira si el objeto esta puesto o no
       drop: function( event, ui ) {
                 $( this )
